2e299bd21cdb77ccf0eb10f86364312297d9ade1
Trevor M <temolano@fullsail.edu>
2018-07-01 23:55:30 -0400

    update 5, validation, add. css, comments, semantic fixes, css fixes

--- js/index.js
+++ js/index.js
@@ -1,18 +1,15 @@
-const key = '57acb95e5d18bf2ef168007fd66dd5ce';
-const api_endpoint = 'https://gateway.marvel.com/v1/public/comics';
-
+const key = "57acb95e5d18bf2ef168007fd66dd5ce";
+const api_endpoint = "https://gateway.marvel.com/v1/public/comics";
 
 //Fetch API data
-
-
 //Filter Comics based off Search Input
 
-const form = document.getElementById('search-form');
+const form = document.getElementById("search-form");
 form.onsubmit = function (ev) {
     ev.preventDefault();
 
-    const filterValue = document.getElementById('searchbar').value.toUpperCase();
-    const inputValue = document.getElementById('searchbar').value;
+    const filterValue = document.getElementById("searchbar").value.toUpperCase();
+    const inputValue = document.getElementById("searchbar").value;
     const url = `${api_endpoint}?titleStartsWith=${filterValue}&limit=40&orderBy=issueNumber%2Ctitle&apikey=${key}`;
 
     fetch(url)
@@ -21,33 +18,30 @@ form.onsubmit = function (ev) {
 
         .then(resultsJSON => {
             //Insert API data
-            let newContent = '';
+            let newContent = "";
             console.log(resultsJSON.data.results);
 
             if(resultsJSON.data.results.length > 20) {
                 load.style.display = "block";
                 for (let i = 0; i < 20; i++) {
                     newContent += '<li class="comicItem"><article>' + '<img src="' + resultsJSON.data.results[i].thumbnail.path + '.jpg"' + ' alt="image of comic art">';
-                    newContent += '<p>' + resultsJSON.data.results[i].title + '</p></article></li>';
+                    newContent += '<h3>' + resultsJSON.data.results[i].title + '</h3></article></li>';
                 }
             }
             else if(resultsJSON.data.results.length <= 20 && resultsJSON.data.results.length > 0) {
                 load.style.display = "none";
                 for (let i = 0; i < resultsJSON.data.results.length; i++) {
                     newContent += '<li class="comicItem"><article>' + '<img src="' + resultsJSON.data.results[i].thumbnail.path + '.jpg"' + ' alt="image of comic art">';
-                    newContent += '<p>' + resultsJSON.data.results[i].title + '</p></article></li>';
+                    newContent += '<h3>' + resultsJSON.data.results[i].title + '</h3></article></li>';
                 }
             }
 
-            else if (resultsJSON.data.results.length === 0) {
-                alert("No results found for that search query");
-            }
-
-            let apiResults = document.querySelector('ul#comicList');
+            let apiResults = document.querySelector("ul#comicList");
             apiResults.innerHTML = newContent;
 
+            //function for results text, transition effects after search
             function inputResults() {
-                let resultsHeader = document.getElementById('results-of-search');
+                let resultsHeader = document.getElementById("results-of-search");
                 resultsHeader.innerHTML = resultsJSON.data.results.length + " Results for \"" + inputValue + "\"";
 
                 let body = document.getElementById("body");
@@ -58,27 +52,25 @@ form.onsubmit = function (ev) {
 
             }
 
-            let li = document.querySelectorAll('.comicItem');
+            let li = document.querySelectorAll(".comicItem");
             for(let i = 0; i < li.length;i++){
 
-                let a = li[i].getElementsByTagName('p')[0];
+                let a = li[i].getElementsByTagName("p")[0];
                 if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
-                    li[i].style.display = '';
+                    li[i].style.display = "";
                 }
 
                 else {
-                    li[i].style.display = 'none';
+                    li[i].style.display = "none";
 
                 }
 
-                inputResults();
-
             }
-
+            inputResults();
         })
 
         .catch( error => {
-            console.log('An Error Occurred:', error);
+            console.log("An Error Occurred:", error);
             alert("Items not available, try again")
         });
 };
@@ -89,8 +81,7 @@ load.addEventListener("click", function () {
 
     load.style.display = "none";
 
-    const filterValue = document.getElementById('searchbar').value.toUpperCase();
-    const inputValue = document.getElementById('searchbar').value;
+    const filterValue = document.getElementById("searchbar").value.toUpperCase();
     const url = `${api_endpoint}?titleStartsWith=${filterValue}&limit=40&orderBy=issueNumber%2Ctitle&apikey=${key}`;
 
     fetch(url)
@@ -104,9 +95,9 @@ load.addEventListener("click", function () {
 
             for (let i = 21; i < 40; i++) {
                 newContent += '<li class="comicItem"><article>' + '<img src="' + resultsJSON.data.results[i].thumbnail.path + '.jpg"' + ' alt="image of comic art">';
-                newContent += '<p>' + resultsJSON.data.results[i].title + '</p></article></li>';
+                newContent += '<h3>' + resultsJSON.data.results[i].title + '</h3></article></li>';
             }
-            let apiResults = document.querySelector('ul#comicList');
+            let apiResults = document.querySelector("ul#comicList");
             apiResults.insertAdjacentHTML("beforeend", newContent);
 
             document.getElementById("comicList").style.height = "fit-content";
@@ -114,6 +105,6 @@ load.addEventListener("click", function () {
         })
 
         .catch(error => {
-            console.log('An Error Occurred:', error)
+            console.log("An Error Occurred:", error)
         });
 });
